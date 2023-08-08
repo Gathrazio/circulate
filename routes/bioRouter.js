0624623646f6d67058d5a641cefaac5993dd1ac2
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-08-07 20:58:18 -0600

    modified bioRouter

--- routes/bioRouter.js
+++ routes/bioRouter.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bioRouter = express.Router();
 const User = require('../models/User.js');
 const Biography = require('../models/Biography.js');
+const Cryptr = require('cryptr');
+const biocryptr = new Cryptr(process.env.BIO_SECRET);
 
-bioRouter.put('/update', (req, res, next) => {
+
+bioRouter.put('/update', (req, res, next) => { // updates bio of current user
     User.findOne({ _id: req.auth._id })
         .then(user => {
             Biography.findOneAndUpdate(
                 { _id: user.bioId },
-                req.body,
+                { body: biocryptr.encrypt(req.body.body)},
                 { new: true })
                 .then(updatedBio => res.status(201).send(updatedBio))
                 .catch(err => {
@@ -19,6 +22,37 @@ bioRouter.put('/update', (req, res, next) => {
         .catch(err => {
             res.status(500)
             return next(new Error("Failed to find the user."))
+        })
+})
+
+bioRouter.get('/', (req, res, next) => { // gets bio of current user
+    User.findOne({ _id: req.auth._id })
+        .then(user => {
+            Biography.findOne({ _id: user.bioId })
+                .then(bio => {
+                    const decryptedBio = {
+                        ...bio._doc,
+                        body: biocryptr.decrypt(bio.body)
+                    }
+                    return res.status(200).send(decryptedBio);
+                })
+                .catch(err => {
+                    res.status(500)
+                    return next(new Error("Failed to find bio."));
+                })
+        })
+        .catch(err => {
+            res.status(500)
+            return next(new Error("Failed to find user."))
+        })
+})
+
+bioRouter.get('/all', (req, res, next) => { // gets the bios of all users
+    Biography.find()
+        .then(bios => res.status(200).send(bios))
+        .catch(err => {
+            res.status(500)
+            return next(new Error("Failed to find bios."))
         })
 })
 
