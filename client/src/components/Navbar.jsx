ec166774c2bcd02911c766aff25d936fc50b82d5
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-08-08 23:21:13 -0600

    client progress

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,49 @@
+import { IconContext } from 'react-icons';
+import { BsFillPersonFill, BsPeopleFill, BsFillChatLeftDotsFill, BsFillChatDotsFill } from 'react-icons/bs'
+import { useState } from 'react'
+
 
 export default function Navbar ({updateToggle}) {
-    const action = (update) => () => updateToggle(update);
+
+    const [highlightToggle, setHighlightToggle] = useState([true, false, false]);
+
+    const action = (update) => () => {
+        if (update === 0) {
+            setHighlightToggle([true, false, false])
+        } else if (update === 1) {
+            setHighlightToggle([false, true, false])
+        } else {
+            setHighlightToggle([false, false, true])
+        }
+        updateToggle(update)
+    }
     return (
         <div className="navbar-wrapper">
             <div className="profile-select">
             <div className="select-text" onClick={action(0)}>
-                    proifle-select
+                    <IconContext.Provider value={{
+                        className: `nav-icons${highlightToggle[0] ? ' highlighted' : ''}`
+                    }}>
+                        <BsFillPersonFill />
+                    </IconContext.Provider>
                 </div>
             </div>
             <div className="chat-select" onClick={action(1)}>
                 <div className="select-text">
-                    chat-select
+                    <IconContext.Provider value={{
+                        className: `nav-icons${highlightToggle[1] ? ' highlighted' : ''}`
+                    }}>
+                        <BsFillChatDotsFill />
+                    </IconContext.Provider>
                 </div>
             </div>
             <div className="friend-select" onClick={action(2)}>
-            <div className="select-text">
-                    friend-select
+                <div className="select-text">
+                    <IconContext.Provider value={{
+                        className: `nav-icons${highlightToggle[2] ? ' highlighted' : ''}`
+                    }}>
+                        <BsPeopleFill />
+                    </IconContext.Provider>
                 </div>
             </div>
         </div>
--- client/src/components/Titlebar.jsx
+++ client/src/components/Titlebar.jsx
@@ -3,6 +3,7 @@ export default function Titlebar () {
     return (
         <div className="titlebar-wrapper">
             <div className="title">CIRCULATE</div>
+            <div className="slogan">A clean and secure messaging service.</div>
         </div>
     )
 }
