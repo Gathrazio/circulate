dcdd639eb3b2f084a0899d73c265e044174e68c0
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-08-09 09:04:14 -0600

    styled profile content

--- client/src/components/ProfileContent.jsx
+++ client/src/components/ProfileContent.jsx
@@ -2,7 +2,24 @@
 export default function ProfileContent () {
     return (
         <div className="profile-content-wrapper content-wrapper">
-            profile content
+            <div className="view-reqs">
+                View active inbound/outbound requests
+            </div>
+            <div className="profile-block">
+                <div className="username-block">
+                    @someone
+                </div>
+                <img className="profile-pic" src="https://image.petmd.com/files/styles/863x625/public/2023-04/kitten-development.jpeg" alt="" />
+                <button className="profile-change-button">Change profile picture</button>
+            </div>
+            <div className="bio-block">
+                <div className="bio-text">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </div>
+            </div>
+            <div className="edit-bio">
+                    Edit bio
+            </div>
         </div>
     )
 }
